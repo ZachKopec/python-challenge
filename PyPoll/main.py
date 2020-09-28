fc70a0fb25254f67ed392dd284cef053f0922cb6
ZachKopec <zachkopec@optonline.net>
2020-09-28 19:50:39 -0400

    Functional loop with results printing to terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,3 +25,17 @@ khanTot = candidate_list.count("Khan")
 correyTot = candidate_list.count("Correy")
 liTot = candidate_list.count("Li")
 oToolTot = candidate_list.count("O'Tooley")
+
+print("Total Votes: " + str(tot_votes))
+print("Khan: " + str((khanTot/tot_votes)*100) + "% " + "(" + str(khanTot) + ")")
+print("Correy: " + str((correyTot/tot_votes)*100) + "% " + "(" + str(correyTot) + ")")
+print("Li: " + str((liTot/tot_votes)*100) + "% " + "(" + str(liTot) + ")")
+print("O'Tooley: " + str((oToolTot/tot_votes)*100) + "% " + "(" + str(oToolTot) + ")")
+if khanTot > correyTot and khanTot > liTot and khanTot > oToolTot:
+    print("Winner: Khan")
+elif correyTot > khanTot and correyTot > liTot and correyTot > oToolTot:
+    print("Winner: Correy")
+elif liTot > khanTot and liTot > correyTot and correyTot > oToolTot:
+    print("Winner: Li")
+elif oToolTot > khanTot and oToolTot > correyTot and oToolTot > liTot:
+    print("Winner: O'Tooley")
